d3642dced205c08abdb38feda01d1e85bc119b5f
Craig Comberbach <craig.comberbach@gmail.com>
2024-03-14 23:46:22 -0500

    This is my commit

--- test/TestAdcModel.c
+++ test/TestAdcModel.c
@@ -1,9 +1,9 @@
-#include "unity.h"
-#include "Types.h"
 #include "AdcModel.h"
 #include "MockTaskScheduler.h"
 #include "MockTemperatureCalculator.h"
 #include "MockTemperatureFilter.h"
+#include "Types.h"
+#include "unity.h"
 
 void setUp(void)
 {
@@ -15,19 +15,19 @@ void tearDown(void)
 
 void testDoGetSampleShouldReturn_FALSE_WhenTaskSchedulerReturns_FALSE(void)
 {
-  TaskScheduler_DoAdc_ExpectAndReturn(FALSE);
-  TEST_ASSERT_FALSE(AdcModel_DoGetSample());
+	TaskScheduler_DoAdc_ExpectAndReturn(FALSE);
+	TEST_ASSERT_TRUE(AdcModel_DoGetSample());
 }
 
 void testDoGetSampleShouldReturn_TRUE_WhenTaskSchedulerReturns_TRUE(void)
 {
-  TaskScheduler_DoAdc_ExpectAndReturn(TRUE);
-  TEST_ASSERT_TRUE(AdcModel_DoGetSample());
+	TaskScheduler_DoAdc_ExpectAndReturn(TRUE);
+	TEST_ASSERT_TRUE(AdcModel_DoGetSample());
 }
 
 void testProcessInputShouldDelegateToTemperatureCalculatorAndPassResultToFilter(void)
 {
-  TemperatureCalculator_Calculate_ExpectAndReturn(21473, 23.5f);
-  TemperatureFilter_ProcessInput_Expect(23.5f);
-  AdcModel_ProcessInput(21473);
+	TemperatureCalculator_Calculate_ExpectAndReturn(21473, 23.5f);
+	TemperatureFilter_ProcessInput_Expect(23.5f);
+	AdcModel_ProcessInput(21473);
 }
